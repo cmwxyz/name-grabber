a8021449bfb8a0f1509acd55b5867a174426cf0e
cmwxyz <cmwxyz@users.noreply.github.com>
2022-01-30 19:47:32 -0800

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject name-grabber "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Takes a string input and returns just the names, first or last."
+  :url "https://github.com/cmwxyz/name-grabber/"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]]
